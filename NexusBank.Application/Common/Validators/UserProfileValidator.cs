8ceee6cffbc999f07262672356da8adccf873f02
Ghostdog02 <alex.vesely07@gmail.com>
2026-06-30 15:12:36 +0300

    Enforce required UserProfile fields in validator
    - Remove conditional When guards from now-required fields
    - Add NotEmpty rules to FirstName, LastName, NationalId, Country

--- NexusBank.Application/Common/Validators/UserProfileValidator.cs
+++ NexusBank.Application/Common/Validators/UserProfileValidator.cs
@@ -8,26 +8,25 @@ public class UserProfileValidator : AbstractValidator<UserProfile>
     public UserProfileValidator()
     {
         RuleFor(p => p.FirstName)
-            .MaximumLength(100)
-            .When(p => p.FirstName is not null);
+            .NotEmpty()
+            .MaximumLength(100);
 
         RuleFor(p => p.LastName)
-            .MaximumLength(100)
-            .When(p => p.LastName is not null);
+            .NotEmpty()
+            .MaximumLength(100);
 
         RuleFor(p => p.DateOfBirth)
             .LessThan(DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-18)))
-            .When(p => p.DateOfBirth is not null)
             .WithMessage("User must be at least 18 years old.");
 
         RuleFor(p => p.NationalId)
-            .MaximumLength(100)
-            .When(p => p.NationalId is not null);
+            .NotEmpty()
+            .MaximumLength(100);
 
         RuleFor(p => p.Country)
+            .NotEmpty()
             .Length(2)
             .Matches("^[A-Z]{2}$")
-            .When(p => p.Country is not null)
             .WithMessage("Country must be a valid ISO 3166-1 alpha-2 code.");
 
         RuleFor(p => p.PostalCode)
