3609d90fa256c9581e46da41d514aa9ad3099543
Ghostdog02 <alex.vesely07@gmail.com>
2026-06-30 12:14:12 +0300

    Add correlation ID middleware
    - Read X-Correlation-ID from incoming request header
    - Generate a new GUID if the header is absent
    - Echo the ID back in the response header
    - Push ID into Serilog LogContext so all request logs carry it

---
+++ NexusBank.Api/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,22 @@
+using Serilog.Context;
+
+namespace NexusBank.Api.Middleware;
+
+public class CorrelationIdMiddleware(RequestDelegate next)
+{
+    private const string HeaderName = "X-Correlation-ID";
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var correlationId = context.Request.Headers[HeaderName].FirstOrDefault()
+                            ?? Guid.NewGuid().ToString();
+
+        context.Items["CorrelationId"] = correlationId;
+        context.Response.Headers[HeaderName] = correlationId;
+
+        using (LogContext.PushProperty("CorrelationId", correlationId))
+        {
+            await next(context);
+        }
+    }
+}
